dd4b0c636661b9ccbbdeb7030f7a5fb233a6f62e
Mark Heaver <mark@citywebconsultants.co.uk>
2017-12-20 13:00:40 +0000

    starting to build out nonce/transaction service. Basic infrastructure in place

--- services/TransactionService.js
+++ services/TransactionService.js
@@ -1,3 +1,15 @@
-var ycTransactionModel = require('./models/ycTransactionModel');
+var ycTransactionModel = require('../models/ycTransactionModel');
+var NonceService = require('../services/NonceService');
 
-class TransactionService{}
+class TransactionService{
+    constructor(){
+        this.nonceService = new NonceService();
+    }
+
+    issueCoins(contractId){
+        let currentNonce = this.nonceService.getCurrentNonceForContract(contractId);
+        console.log("current nonce: " + currentNonce);
+    }
+}
+
+module.exports = TransactionService;
