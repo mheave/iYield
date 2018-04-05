17ac0ae3f07db82fcf5f7b55397f469cf06b7341
Mark Heaver <mark@citywebconsultants.co.uk>
2018-04-05 12:32:13 +0100

    adding tests for services

---
+++ tests/test-ConfigurationService.js
@@ -0,0 +1,27 @@
+import test from 'ava';
+const ConfigurationService = require('../services/ConfigurationService');
+
+var _configService;
+
+test('can create new instance of service', t => {
+	t.notThrows(() => new ConfigurationService());
+});
+
+test.serial.before(t => {
+    _configService = new ConfigurationService();
+});
+
+test('can get global settings object', async t => {
+	let globalSettings = _configService.getGlobalSettings();
+	t.not(globalSettings, null);
+});
+
+test('can get local storage settings and contracts', t => {
+	let localStorageSettings = _configService.getLocalStorageSettings();
+	t.not(localStorageSettings.currentContractsSettings, null);
+});
+
+
+
+
+
